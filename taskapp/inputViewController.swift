bc0187bab43244ff56a49e36793e30a72ea37624
ryotaro tsuji <tsuji.xs@au.com>
2020-02-09 10:16:33 +0900

    pod installと.gitignoreの作成まで実施

---
+++ taskapp/inputViewController.swift
@@ -0,0 +1,25 @@
+//
+//  inputViewController.swift
+//  taskapp
+//
+//  Created by Apple on 2020/02/05.
+//  Copyright © 2020 ryotaro.tsuji. All rights reserved.
+//
+
+import UIKit
+
+class inputViewController: UIViewController {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentsTxtView: UITextView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
